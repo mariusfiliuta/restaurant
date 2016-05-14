25aee5bc9214f7ce980f2cde430355a67e071513
Honceriu Mihai <mihaihonceriu@yahoo.com>
2016-05-14 19:35:04 +0300

    no message

--- src/main/resources/db/migration/V2_POPULATE_CATEGORIES.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `foame`.`categories` (`id`, `name`) VALUES ('1', 'Pizza');
-INSERT INTO `foame`.`categories` (`id`, `name`) VALUES ('2', 'Drinks');
-INSERT INTO `foame`.`categories` (`id`, `name`) VALUES ('3', 'Snacks');
-INSERT INTO `foame`.`categories` (`id`, `name`) VALUES ('4', 'Pasta');
