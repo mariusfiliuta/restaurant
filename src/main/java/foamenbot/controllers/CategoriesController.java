0151d4e87f329c59f763484e7e931a4116ceddfc
Vlad Olaru <vladutandrei.olaru@gmail.com>
2016-05-29 19:03:52 +0300

    Started working on product view

--- src/main/java/foamenbot/controllers/CategoriesController.java
+++ src/main/java/foamenbot/controllers/CategoriesController.java
@@ -10,7 +10,9 @@ import foamenbot.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import foamenbot.services.CategoryService;
@@ -22,6 +24,9 @@ public class CategoriesController {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private ProductService productService;
 
     @ModelAttribute("currentUser")
     private User getCurrentUser() {
@@ -42,5 +47,14 @@ public class CategoriesController {
     @RequestMapping(value = {"/categories"}, method = RequestMethod.GET)
     public String getCategoriesPage() {
         return "categories";
+    }
+
+    @RequestMapping(value = {"/categories/{id}"}, method = RequestMethod.GET)
+    public String getProductsForEachCategory(@PathVariable long id, Model model) {
+        Category currentCategory = categoryService.findById(id);
+        List<Product> products = productService.findByCategory(currentCategory);
+        model.addAttribute("category", currentCategory);
+        model.addAttribute("products", products);
+        return "productsView";
     }
 }
--- src/main/java/foamenbot/services/ProductService.java
+++ src/main/java/foamenbot/services/ProductService.java
@@ -1,5 +1,6 @@
 package foamenbot.services;
 
+import foamenbot.model.Category;
 import foamenbot.model.Product;
 
 import java.util.List;
@@ -10,5 +11,6 @@ public interface ProductService {
     void save(Product product);
     void delete(Product product);
     Product findById(long id);
+    List<Product> findByCategory(Category category);
     boolean isInStock(Product product);
 }
